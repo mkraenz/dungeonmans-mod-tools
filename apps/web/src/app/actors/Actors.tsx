9c2a478ab641077c446e728ec9c164491ef76304
Mirco Kraenz <software@kraenz.eu>
2026-08-09 17:55:33 +0200

    fix(web): remove background size cover

--- apps/web/src/app/actors/Actors.tsx
+++ apps/web/src/app/actors/Actors.tsx
@@ -32,7 +32,6 @@ const Portrait: FC<PortraitProps> = ({ actor, sprite, textures }) => {
         className={styles.actorPortrait}
         style={{
           backgroundImage: `url('${dataUrl}')`,
-          backgroundSize: 'cover',
           // background position is negative because it basically just moves the top-left corner. The overlap with the container div is then what is visible.
           // TODO verify whether y needs to be negative or not.
           backgroundPosition: `${-sprite.xloc}px ${-sprite.yloc}px`,
